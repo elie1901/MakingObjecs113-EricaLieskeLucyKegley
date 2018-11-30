1d89b301a1a1ca34d455a78253c7164424bf69aa
Erica Lieske <42978205+elie1901@users.noreply.github.com>
2018-11-30 13:09:02 -0600

    Add files via upload

---
+++ MakingObjecs113-EricaLieskeLucyKegley-master/ArrayMediaLib.java
@@ -0,0 +1,88 @@
+public class ArrayMediaLib
+{
+  public static void main(String getTitle)
+   {
+       {Song[] topTenSongs = {new Song(),
+           new Song(),
+           new Song()
+          
+                         
+                       
+                        };
+       /*{String[] getTitle = {("Smooth"),
+                         ("Mack the Knife"),
+                         ("How do I live")
+                         
+                        };
+                    */
+     for (Song s: topTenSongs)
+    {
+        System.out.println(s.getTitle);
+    }
+}
+}
+}
+}
+
+    
+
+    
+    /*public static void main()
+   {
+       {
+        String[] sharingFriends = {"Brian",
+                                   "Michele",
+                                   "Jenna",
+                                   "Erica",
+                                   "Garrett",
+                                   "Erin"
+        };
+
+   
+ 
+
+    for (int i = 0; i < sharingFriends.length; i++){
+               System.out.println(sharingFriends[i]);
+      {   
+        sharingFriends[i] = sharingFriends[i];
+      } // end for loops
+    }//end method
+   } 
+   
+   public static double getAvg(int[] daysBtwnPurchase)
+      {
+        double total = 0;
+        for (int day : daysBtwnPurchase)
+        {
+            total = total + day;
+        }
+        return total / daysBtwnPurchase.length;
+      }
+    
+   public static void main()
+    {
+    int[] daysBtwnPurchase = {2, 5, 1, 2, 4, 1, 3};
+    System.out.println(getAvg(daysBtwnPurchase));
+    }
+}
+*/
+
+    
+        
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+
+ 
+
+
+
+
